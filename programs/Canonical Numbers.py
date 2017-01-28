f25b16868baa45e026957c812efcf01cb686a2f3
Brian <brian.a.matthys@gmail.com>
2017-01-27 23:32:57 -0500

    first variables

---
+++ programs/Canonical Numbers.py
@@ -0,0 +1,24 @@
+#This program will print numbers from 1 to 1000
+# and with their correct ending such as "st", "nd", "rd", or "th"
+
+numbers = []
+
+for i in range(1, 1001):
+    numbers.append(i)
+
+ending = ""
+
+for number in numbers:
+    if number % 10 == 1:
+        ending = "st"
+
+    elif number % 10 == 2:
+        ending = "nd"
+
+    elif number % 10 == 3:
+        ending = "rd"
+
+    else:
+        ending = "th"
+
+    print(str(number) + ending)
