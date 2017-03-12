fb7fbd70e3a311961158dbca04e99c1a3cab599c
Brian <brian.a.matthys@gmail.com>
2017-03-12 17:54:40 -0400

    Visualize rolling a die

--- Data Visualization/die_visual.py
+++ Data Visualization/die_visual.py
@@ -1,18 +1,31 @@
+import pygal
 from die import Die
 
-# Create a D6
-die = Die()
+# Create a D6  and a D10
+die_1 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store the results in a list.
 results = []
-for roll in range(1000):
-    result = die.roll()
+for roll in range(50000):
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# Visualize the results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling a D6 and D10 50,000 times."
+hist.x_labels = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
+                 "13", "14", "15", "16"]
+hist.x_title = "Result"
+hist.y_title = "Frequency of result"
+
+hist.add("D6 + D10", frequencies)
+hist.render_to_file("die_visual.svg")
