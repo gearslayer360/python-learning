35fb4fabfa8ff3ff69799456071cab100a7f2482
Brian <brian.a.matthys@gmail.com>
2017-03-23 23:16:48 -0400

    Find all ways to spend exactly 5 dollars on fruit

--- programs/jenny_fruit_basket_challenge.py
+++ programs/jenny_fruit_basket_challenge.py
@@ -7,57 +7,48 @@
 
 from math import floor
 
-fruits = {}  # Dict of fruits and prices
+fruits = {
+    'apple': 59,
+    'banana': 32,
+    'coconut': 155,
+    'grapefruit': 128,
+    'jackfruit': 1100,
+    'kiwi': 41,
+    'lemon': 70,
+    'mango': 97,
+    'orange': 73,
+    'papaya': 254,
+    'pear': 37,
+    'pineapple': 399,
+    'watermelon': 500
+}
+
 max_price = 500  # total money we have to spend
-max_fruit = []  # maximum number of fruits we can afford for each fruit
-answers = []  # solution
-fruit_names = []  # list of only names of fruit to reference as a dict key
+solutions = 0  # total number of solutions
 
 
-def prompt():
-    """Ask the user for the fruits and their prices"""
-    while True:
-        answer = input(print("Enter a fruit name and price, or enter q to quit:"))
-
-        if answer == "q":
-            break
-        else:
-            parts = answer.split()
-            fruits[parts[0]] = parts[1]
-
-
-def calculate(curr_price, answer, num):
+def calculate(curr_price, basket, fruit_list):
     """Defining a function to solve for the combinations of fruits possible
     using a recursive solution"""
-    if num == 0:
-        for k, v in fruits.items():
-            max_fruit.insert(0, floor(500 / int(fruits[k])))
-            fruit_names.append(k)
-        max_fruit.sort()
-        max_fruit.reverse()
-        fruit_names.sort()
 
-    print("current price " + str(curr_price))  # test string remove later
+    global solutions
 
     if curr_price == max_price:  # If the price is a perfect match, add the answer combo to list of answers and delete current fruit
-        return answers.append(answer)
-        del max_fruit[0]
+        print(basket + str(curr_price))
+        solutions += 1
     elif curr_price < max_price:  # If the prices don't match, keep adding fruits to the combo and incrementing the price
-        for i in range(0, max_fruit[0] - 1):
-            print(i)
-            if num + 1 < len(fruit_names):  # While there are still fruits to go through in the list
-                if i == 0:  # Don't take one of the fruits
-                    print("I'm the first")  # test string remove later
-                    return calculate((curr_price + i * int(fruits[fruit_names[0]])), answer, num + 1)
-                elif i == 1:  # Take only one of the fruits
-                    print("I'm the second")  # test string remove later
-                    return calculate((curr_price + i * int(fruits[fruit_names[0]])), answer + fruit_names[0] + " " + str(i) + ",", num + 1)
-                else:  # Taking more than one of the fruits
-                    print("I'm the third")  # test string remove later
-                    return calculate((curr_price + i * int(fruits[fruit_names[0]])), answer + fruit_names[0] + "s" + str(i) + ",", num + 1)
-    else:
-        pass
-    return print(answers)
+        while len(fruit_list) > 0:
+            fruit = fruit_list.pop()
+            cost = fruits[fruit]
+            nfruit = int((max_price - curr_price) / cost)
 
-prompt()
-calculate(0, " ", 0)
+            for i in range(1, nfruit + 1):
+                newList = list(fruit_list)
+                newBasket = basket + str(i) + " " + fruit
+                if i > 1:
+                    newBasket += "s"
+                newBasket += ", "
+                calculate(curr_price+(i * cost), newBasket, newList)
+
+calculate(0, " ", list(fruits.keys()))
+print("found " + str(solutions) + " solutions, for a budget of " + str(max_price))
