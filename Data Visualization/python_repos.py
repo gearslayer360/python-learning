e10570834b06668704bc5109da47771eee0ec1e1
Brian <brian.a.matthys@gmail.com>
2017-03-26 23:09:53 -0400

    update to fix old code

--- Data Visualization/python_repos.py
+++ Data Visualization/python_repos.py
@@ -22,13 +22,13 @@ for repo_dict in repo_dicts:
 
 # Make visualization
 my_style = LS("#333366", base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
 
 my_config = pygal.Config()
 my_config.x_label_rotation = 45
 my_config.show_legend = False
-my_config.title_font_size = 24
-my_config.label_font_size = 14
-my_config.major_label_font_size = 18
 my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
