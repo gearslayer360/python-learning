0dc57b5eb557b9259cd6fcb4ca4504db28c38bab
Brian <brian.a.matthys@gmail.com>
2017-04-08 21:36:35 -0400

    Learning Log project

---
+++ learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\brian\pycharmprojects\python-learning\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
